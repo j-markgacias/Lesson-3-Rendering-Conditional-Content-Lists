b89d8c284166f7dfc4050e19c76f56e08553d403
majorjupiter <mjfuencias@gmail.com>
2021-11-17 09:53:14 +0800

    Created and Connected Vue App Instances.

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ document.addEventListener('mousemove', (e) =>
     cursor.style.left = e.pageX + 'px';
     cursor.style.top = e.pageY + 'px';
 })
+
+const app = Vue.createApp({
+  
+    
+});
+
+app.mount('#assignment');
